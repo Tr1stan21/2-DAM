50df36ab43d355ee77eadbda6c3eede021cab811
Tr1stan21 <javichd21@gmail.com>
2026-02-12 13:38:59 +0100

    clase //2026

--- Acceso a Datos/T_Final_25-26/src/main/java/bootstrap/MainApp.java
+++ Acceso a Datos/T_Final_25-26/src/main/java/bootstrap/MainApp.java
@@ -17,10 +17,43 @@ public class MainApp {
 
             HibernateDeportistaRepository deportistaRepo = new HibernateDeportistaRepository(session);
 
-            System.out.println("CRUD Deportista\n");
+            System.out.println("--- CRUD DEPORTISTA ---\n");
 
             Deportista deportista = new Deportista("12345678P", "Usain", "Bolt");
             System.out.println("Deportista guardado: " + deportistaRepo.save(deportista));
+
+            System.out.println("Deportista por id: "+ deportistaRepo.findById("12345678P"));
+
+            System.out.println("Todos los deportistas: ");
+            for(Deportista d : deportistaRepo.findAll()) {
+                System.out.println(d);
+            }
+
+            deportista.setNombre("Jose");
+            deportista.setApellido("Pérez");
+            System.out.println("Deportista actualizado: "+ deportistaRepo.update(deportista));
+
+            System.out.println("Deportista eliminado correctamente: "+ (deportistaRepo.deleteById("12345678P")? "true" : "false"));
+
+
+            System.out.println("\n\n--- CONSULTAS ---");
+
+            System.out.println("\nEntrenamientos con deportistas: ");
+            for(Object[] obj : getEntrenamientosDeportistas(session)) {
+                for (Object o : obj) {
+                    System.out.println(o);
+                }
+            }
+
+            System.out.println("\nEntrenamientos sin deportistas: ");
+            for(Entrenamiento e : getEntrenamientosSinDeportistas(session)) {
+                System.out.println(e);
+            }
+
+            System.out.println("\nEntrenamientos con más de X deportistas: ");
+            for(Entrenamiento e : getEntrenamientosConMasDeXDeportistas(session, 1)) {
+                System.out.println(e);
+            }
 
         }
     }
@@ -32,14 +65,23 @@ public class MainApp {
     }
 
     private static List<Entrenamiento> getEntrenamientosSinDeportistas (Session session) {
-        String hql = "from Entrenamiento e left join e.deportistas d where d is empty";
+        String hql = "from Entrenamiento e where e.deportistas is empty";
 
         return session.createQuery(hql, Entrenamiento.class).getResultList();
     }
 
     private static List<Entrenamiento> getEntrenamientosConMasDeXDeportistas (Session session, int numDeportistas) {
-        String hql = "from Entrenamiento e join e.deportistas d group by e having count (d) > :numDeportistas";
+        String hql = "from Entrenamiento e join e.deportistas d group by e having count(d) > :numDeportistas";
+
+        return session.createQuery(hql, Entrenamiento.class).setParameter("numDeportistas", numDeportistas).getResultList();
 
     }
+
+//    private static List<Object[]> getEntrenamientoMasPopularConDetalles (Session session) {
+//        String hql = "from Entrenamiento e join e.deportistas d group by e where count(d) > ";
+//
+//        return session.createQuery(hql, Object[].class).getResultList();
+//    }
+
 
 }
